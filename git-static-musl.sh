bb2b11b5855e16c63d6bcd114a4b30d5c86eaf6d
Matt Rinsch <gfunkmonk@mailbox.org>
2026-03-08 14:37:47 -0400

    ggggggggggggggggg

--- git-static-musl.sh
+++ git-static-musl.sh
@@ -150,12 +150,9 @@ cd git-${GIT_VERSION}/ && \
   CFLAGS='-Os -ffunction-sections -fdata-sections -Wno-unterminated-string-initialization \
   -I/usr/include/x86_64-linux-gnu -isystem /usr/include/mit-krb5 -I/usr/include/p11-kit-1' && \
 CC=clang make NO_PERL=1 RUNTIME_PREFIX=1 -j\$(nproc) && \
+if [ ! -f "git" ]; then echo -e "${TOMATO}Error: git binary not found after build${NC}" >&2 && exit 1; fi && \
 strip git && \
 upx --lzma git"
-if [ ! -f "git" ]; then
-  echo -e "${TOMATO}Error: git binary not found after build${NC}" >&2
-  exit 1
-fi
 mkdir -p dist
 cp "./pasta/git-${GIT_VERSION}/git" "dist/git-${ARCH}"
 if command -v file >/dev/null 2>&1; then echo -e "${ORANGE} File Info:  $(file "dist/git-${ARCH}" | cut -d: -f2-)${NC}"; fi
